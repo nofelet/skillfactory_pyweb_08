c15078f9b3c18b9b63f3e8685f988f378844d259
Nikolay Filippov <nph@rambler.ru>
2019-02-26 09:01:29 +0300

    Modify server_08.py for heroku

--- server_08.py
+++ server_08.py
@@ -49,8 +49,7 @@ def api_test():
 #	"x": x,
 #	}
 
-run(
-  host="localhost",
-  port=8080,
-  autoreload=True
-)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
